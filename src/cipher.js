d1680aba0a798f9e2821d27976376ccfe9873c01
maridiaz <maritzadiaz92@gmail.com>
2019-01-14 22:41:33 -0600

    cambio de páginas, botones siguen instrucciones

--- src/cipher.js
+++ src/cipher.js
@@ -1,11 +1,7 @@
 window.cipher = {
   // ... 
 };
-<<<<<<< HEAD
-enconde: () => {},
-decode: () => {}
-=======
 enconde: () => {,
 decode: () => 
 }
->>>>>>> 3324184f0d914b464c594c7795caee6a2fd41c43
+
--- src/index.js
+++ src/index.js
@@ -1,18 +1,53 @@
 //cambio de páginas
 const botonUno = document.getElementById('terminos');
-
-botonUno.addEventListener('click',botonA);
-function botonA(){
+    botonUno.addEventListener('click',botonA);
+    function botonA(){
     welcome.classList.add("hideElement");
     condiciones.classList.remove("hideElement");
     }
     
-botonDos = document.getElementById('registro');
-
-botonDos.addEventListener('click',botonB);
+const botonDos = document.getElementById('condiciones');
+    botonDos.addEventListener('click',botonB);
     function botonB(){
+    condiciones.classList.add("hideElement");
+    welcome.classList.remove("hideElement");
+    }
+
+const botonTres = document.getElementById('regresar1')
+    botonTres.addEventListener('click',botonC)
+    function botonC(){
     welcome.classList.add("hideElement");
-    registro.classList.remove("hideElement");
+    condiciones.classList.remove("hideElement");
+    }
+
+const botonCuatro = document.getElementById('registro');
+    botonCuatro.addEventListener('click',botonD);
+    function botonD(){
+    welcome.classList.add("hideElement");
+    registrarse.classList.remove("hideElement"); 
+    
+const botonCinco = document.getElementById('regresar2');
+    botonCinco.addEventListener('click',botonE);
+    function botonE(){
+    welcome.classList.remove("hideElement");
+    condiciones.classList.add("hideElement");
+    registrarse.classList.add("hideElement");
+
+//
+const botonSeis = document.getElementById('enviar1');
+    botonSeis.addEventListener('click', botonF);
+    function botonF(){
+    registrarse.classList.add("hideElement");
+    final.classList.remove("hideElement");
+
+    }
+    
+    
+    
+
+
+    
+    }
     }
 
 
