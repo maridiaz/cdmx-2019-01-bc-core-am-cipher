6db5bc470978b815fc8437ee07275a0456c995df
maridiaz <maritzadiaz92@gmail.com>
2019-01-23 14:38:18 -0600

    mejora en la estructura del js, dry aplicado

--- src/index.js
+++ src/index.js
@@ -1,82 +1,60 @@
 //cambio de páginas a través de botones
 //add(hideElement) añade esconder :. oculta
 //remove(hideElement) quita esconder :. muestra
-//botón en página de inicio que lleva a la página de términos y condiciones
-const botonUno = document.getElementById('terminos');
-botonUno.addEventListener('click', botonA);
 
-function botonA() {
-  const welcome = document.getElementById("welcome");
+//botones por los cuales se realiza el cambio de página
+const botonUno = document.getElementById("terminos");
+const botonDos = document.getElementById("condiciones");
+const botonTres = document.getElementById("regresar1");
+const botonCuatro = document.getElementById("registro");
+const botonCinco = document.getElementById("regresar2");
+const botonSeis = document.getElementById("enviar1");
+const botonSiete = document.getElementById("terminar1");
+const welcome = document.getElementById("welcome");
+const condiciones = document.getElementById("condiciones");
+
+//botón en página de inicio que lleva a la página de términos y condiciones
+botonUno.addEventListener("click", () => {
   welcome.classList.add("hideElement");
   condiciones.classList.remove("hideElement");
-}
+});
 
-//botón en página de términos que lleva a página de inicio    
-const botonDos = document.getElementById('condiciones');
-const condiciones = document.getElementById("condiciones");
-botonDos.addEventListener('click', botonB);
-
-function botonB() {
-  const condiciones = document.getElementById("condiciones");
-  const welcome = document.getElementById("welcome");
+//botón en página de términos que lleva a página de inicio         
+botonDos.addEventListener("click", () => {
   condiciones.classList.add("hideElement");
   welcome.classList.remove("hideElement");
-}
+});
 
 //botón en página de inicio que lleva a la página de registro
-const botonTres = document.getElementById('regresar1')
-botonTres.addEventListener('click', botonC)
-
-function botonC() {
-  const welcome = document.getElementById("welcome");
+botonTres.addEventListener("click", () => {
   welcome.classList.add("hideElement");
   condiciones.classList.remove("hideElement");
-}
+});
 
 //botón en página de registro que lleva página de inicio
-const botonCuatro = document.getElementById('registro');
-botonCuatro.addEventListener('click', botonD);
-
-function botonD() {
-  const welcome = document.getElementById("welcome");
-  const registrarse = document.getElementById("registrarse");
+botonCuatro.addEventListener("click", () => {
   welcome.classList.add("hideElement");
   registrarse.classList.remove("hideElement");
-}
+});
 
-//botón en página de registro que lleva a la página donde está el formulario/cifrado
-const botonCinco = document.getElementById('regresar2');
-botonCinco.addEventListener('click', botonE);
-
-function botonE() {
-  const welcome = document.getElementById("welcome");
-  const registrarse = document.getElementById("registrarse");
+//botón en página de registro que lleva a la página donde está el formulario/cifrado 
+botonCinco.addEventListener('click', () => {
   welcome.classList.remove("hideElement");
   condiciones.classList.add("hideElement");
   registrarse.classList.add("hideElement");
-}
+});
 
-//botón en página del formulario/cifrado que lleva a página final
-const botonSeis = document.getElementById('enviar1');
-botonSeis.addEventListener('click', botonF);
-
-function botonF() {
-  const registrarse = document.getElementById("registrarse");
-  const final = document.getElementById("final");
+//botón en página del formulario/cifrado que lleva a página final   
+botonSeis.addEventListener('click', () => {
   registrarse.classList.add("hideElement");
   final.classList.remove("hideElement");
-}
+});
 
 //botón en página final que lleve a página de inicio
-const botonSiete = document.getElementById('terminar1');
-botonSiete.addEventListener('click', botonG);
-
-function botonG() {
-  const welcome = document.getElementById("welcome");
-  const final = document.getElementById("final");
+botonSiete.addEventListener("click", () => {
   final.classList.add("hideElement");
   welcome.classList.remove("hideElement");
-}
+});
 
 //caja de texto a cifrar y caja descifrar
 //el nombre de los id están en HTML, no regresa lo que se ingresa en input
@@ -88,19 +66,15 @@ const listaUno = document.getElementById('cifrado2');
 const listaDos = document.getElementById('descifrado2');
 
 //boton cifrar
-botonCifrar.addEventListener('click',()=>{
+botonCifrar.addEventListener('click', () => {
   let offset = parseInt(listaUno.value);
   let string = inputEncode.value;
-  document.getElementById('message1').innerHTML=window.cipher.encode(offset, string);
-
+  document.getElementById('message1').innerHTML = window.cipher.encode(offset, string);
 })
 
 //boton descifrar
-botonDescifrar.addEventListener('click',()=>{
+botonDescifrar.addEventListener('click', () => {
   let offset = parseInt(listaDos.value);
   let string = inputDecode.value;
-  document.getElementById('message2').innerHTML=window.cipher.decode(offset, string);
-
+  document.getElementById('message2').innerHTML = window.cipher.decode(offset, string);
 })
-    
-
